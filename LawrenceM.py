d066724035f2e8e46f33304559fc604a1a9d192f
lawrencemacalalad <69102093+lawrencemacalalad@users.noreply.github.com>
2020-08-03 15:23:27 +0800

    Delete LawrenceM.py

--- LawrenceM.py
+++
@@ -1,9 +0,0 @@
-
-print("Name: Lawrence S. Macalalad")
-print("Email: lawrencesevillamacalalad@gmail.com")
-print("Language: Python")
-print("Biostack: Genomics")
-print("Slack Username: @LawrenceM")
-
-
-input("Press any key to exit")
