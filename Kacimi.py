a240fedec6c01cea414d8f27571360c2f7f8a19c
minou <minou@MacBook-Air-de-macbook.local>
2020-08-03 13:28:58 +0100

    remove

--- Kacimi.py
+++
@@ -1,5 +0,0 @@
-print("Full Name: Salah Eddine Oussama Kacimi")
-print("Email Adress: Kacimi.oussama97@yahoo.com")
-print("Language: Python")
-print("Biostack: Drug Development")
-print("Slack Username: @Kacimi")
