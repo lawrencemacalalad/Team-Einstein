c7098f98efca9254a6d70add193d32841fff3f8e
Abdulrokeeb093 <69093793+Abdulrokeeb093@users.noreply.github.com>
2020-08-03 02:42:56 +0100

    Python script uploaded
    Hi Lawrence, I just added my python file containing my script to the task to the repository.

---
+++ Abdulrokeeb Task 0.py
@@ -0,0 +1,5 @@
+print('Name: Abdulrokeeb Jakkari')
+print('Email: abdulroqeebjakkari@yahoo.com')
+print('Language used: Python')
+print('Biostack: Vaccine Informatics')
+print('Slack username: @Abdulrokeeb')
