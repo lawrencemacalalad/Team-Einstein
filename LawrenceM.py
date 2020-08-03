7f5dbbd702347de7d2ccf319366789a87f4247a5
lawrencemacalalad <69102093+lawrencemacalalad@users.noreply.github.com>
2020-08-03 15:55:36 +0800

    Add files via upload

---
+++ LawrenceM.py
@@ -0,0 +1,9 @@
+
+name = 'Lawrence S. Macalalad'
+email = 'lawrencesevillamacalalad@gmail.com'
+language = 'Python'
+biostack = 'Genomics'
+slack = '@LawrenceM'
+
+print("Name: {}\nEmail: {}\nLanguage: {}\nBiostack: {}\nSlack Username: {}".format(name, email, language, biostack, slack))
+
