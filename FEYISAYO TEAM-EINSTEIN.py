eecfa8aecee3957a540f698a62a3be359528849a
Feyi24 <69109764+Feyi24@users.noreply.github.com>
2020-08-04 21:32:11 +0100

    Add files via upload

---
+++ FEYISAYO TEAM-EINSTEIN.py
@@ -0,0 +1,7 @@
+print ("Name : Ayoola Oluwafeyisayo Elizabeth")
+print ("Email : lizalaide24@gmail.com")
+print ("Language used : Python")
+print ("Biostack : Genomics and data analysis")
+print ("Username : @Feyi24")
+
+
