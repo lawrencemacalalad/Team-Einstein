5ac58407aaeb812cba8a6664df493eacf357eabd
lawrencemacalalad <69102093+lawrencemacalalad@users.noreply.github.com>
2020-08-05 22:07:08 +0800

    Add files via upload

---
+++ TeamEinsteinBash.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git clone https://github.com/lawrencemacalalad/TeamEinstein.git && cd ./TeamEinstein &&py LawrenceM.py >> Einstein.csv &&py Adetutu.py >> Einstein.csv &&py Kacimi_Drug_Develpment.py >> Einstein.csv
