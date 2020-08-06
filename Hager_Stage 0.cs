510b271633ab3a8f8c266d74c2b422ffb7ffecfa
hajer772 <69155232+hajer772@users.noreply.github.com>
2020-08-06 15:06:18 +0200

    Add files via upload

---
+++ Hager_Stage 0.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hager_Stage_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Name = "Hager Mahmoud \n\r";
+            String Address = "Hagerashry0@gmail.com \n\r";
+            String Language = "C# \n\r";
+            String Biostack = "Software Development \n\r";
+            String Slack = "@Hajer \n\r ";
+
+
+            Console.WriteLine("Name : "+Name + "\n\r" + "Address : " + Address+"\n\r" + "Language : " + Language + "\n\r" + "Biostack : " + Biostack + "\n\r" + "Slack : " + Slack);
+        }
+    }
+}
