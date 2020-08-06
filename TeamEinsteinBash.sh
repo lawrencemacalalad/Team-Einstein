37abeccecd5e0c7ac3631d351deedfab2217ac9a
lawrencemacalalad <69102093+lawrencemacalalad@users.noreply.github.com>
2020-08-06 23:59:15 +0800

    Update TeamEinsteinBash.sh

--- TeamEinsteinBash.sh
+++ TeamEinsteinBash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/lawrencemacalalad/TeamEinstein.git && cd ./TeamEinstein &&py LawrenceM.py >> Einstein.csv &&py Adetutu.py >> Einstein.csv &&py Kacimi_Drug_Develpment.py >> Einstein.csv &&py Ihechi.html.py >> Einstein.csv &&js Yakumbur.js >> Einstein.csv &&py Internship.py >> Einstein.csv &&php Deborah.php >> Einstein.csv &&py feyisayo.py >> Einstein.csv &&js olalekan.js >> Einstein.csv &&py ekene_ezeunala_@David.py >> Einstein.csv
+git clone https://github.com/lawrencemacalalad/TeamEinstein.git && cd ./TeamEinstein &&py LawrenceM.py >> Einstein.csv &&py Adetutu.py >> Einstein.csv &&py Kacimi_Drug_Develpment.py >> Einstein.csv &&py Menna.py >> Einstein.csv &&py Ihechi.html.py >> Einstein.csv &&js Yakumbur.js >> Einstein.csv &&py Internship.py >> Einstein.csv &&php Deborah.php >> Einstein.csv &&py feyisayo.py >> Einstein.csv &&js olalekan.js >> Einstein.csv &&py ekene_ezeunala_@David.py >> Einstein.csv
 &&sh usmood_stage_0.sh >> Einstein.csv
 for file in $(ls) ;
 do
