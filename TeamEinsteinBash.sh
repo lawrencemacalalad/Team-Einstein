720a3e71a1b12433557fbda3f83e41c252317a31
agosileoluwafemi <69110803+agosileoluwafemi@users.noreply.github.com>
2020-08-06 14:30:50 +0100

    Update TeamEinsteinBash.sh

--- TeamEinsteinBash.sh
+++ TeamEinsteinBash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git clone https://github.com/lawrencemacalalad/TeamEinstein.git && cd ./TeamEinstein &&py LawrenceM.py >> Einstein.csv &&py Adetutu.py >> Einstein.csv &&py Kacimi_Drug_Develpment.py >> Einstein.csv &&py Ihechi.html.py >> Einstein.csv &&js Yakumbur.js >> Einstein.csv &&php Deborah.php >> Einstein.csv &&py feyisayo.py >> Einstein.csv &&js olalekan.js >> Einstein.csv &&py ekene_ezeunala_@David.py >> Einstein.csv
-&&sh usmood_stage_0.sh >> Einstein.csv
+&&sh usmood_stage_0.sh >> Einstein.csv &&py internship.py ›› Einstein.csv
