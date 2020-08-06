4d1f8f6086d8714f16604b6994f571fc57e34ce1
salma435 <69112305+salma435@users.noreply.github.com>
2020-08-06 19:31:59 +0200

    Add files via upload

---
+++ Salma_stage0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+            string Name = "Salma Ali Zein \n\r";
+            string Address = "loma50446@gmail.com \n\r";
+            string Language = "C++ \n\r";
+            string Biostack = "Software Development \n\r";
+            string Slack = "@Salma Ali Zein \n\r ";
+
+
+            cout <<"Name : "<< Name << "\n\r" ;
+            cout << "Address : " << Address<<"\n\r";
+            cout << "Language : " << Language << "\n\r";
+            cout << "Biostack : " << Biostack << "\n\r" ;
+            cout <<  "Slack : " << Slack << endl;
+    return 0;
+}
