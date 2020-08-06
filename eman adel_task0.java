ed849b816273f4eab2242130e3903ec38742223d
emanadel0 <64305404+emanadel0@users.noreply.github.com>
2020-08-06 15:43:29 +0200

    Add files via upload

---
+++ eman adel_task0.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        String Name = "Eman Adel \n\r";
+        String Address = "emansultan990@gmail.com \n\r";
+        String Language = "Java \n\r";
+        String Biostack = "Software Development \n\r";
+        String Slack = "@Eman Adel Salem \n\r ";
+
+        System.out.print("Name : "+Name + "\n\r" + "Address : " + Address+"\n\r" + "Language : " + Language + "\n\r" + "Biostack : " + Biostack + "\n\r" + "Slack : " + Slack);
+    }
+}
